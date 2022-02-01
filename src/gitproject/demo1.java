fdbb7b805c073996387384a267d14596ba0fc08d
Shailendrakumar90 <shailendrahasanmukhi@gmail.com>
2022-02-01 15:46:54 +0530

    this is my second commit

---
+++ src/gitproject/demo1.java
@@ -0,0 +1,14 @@
+package gitproject;
+
+import java.lang.invoke.MethodHandles.Lookup.ClassOption;
+
+public class demo1 {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("demo prrogramme");
+	}
+	
+	
+
+}
---
+++ src/gitproject/demo2.java
@@ -0,0 +1,12 @@
+package gitproject;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("demo2 programme");
+
+	}
+
+}
